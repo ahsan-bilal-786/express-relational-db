4b10429286e76a34cfa855d6572949db1546ac35
Ahsan Bilal <engr.ahsan.bilal@gmail.com>
2020-05-20 19:48:29 +0500

    Incorporating json web tokens to generate user token in signup

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,36 @@
-var { User } = require("../models");
+const JWT = require("jsonwebtoken");
+const { User } = require("../models");
 
-const createUser = (req, res, next) => {
-  return User.create(req.body)
-    .then((data) => {
-      return res.json(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the Session.",
-      });
-    });
+const signToken = (sub) => {
+  return JWT.sign(
+    {
+      iss: "express-relational-db",
+      sub,
+      iat: new Date().getTime(), // current time
+      exp: new Date().setDate(new Date().getDate() + 1), // current time + 1 day ahead
+    },
+    process.env.AUTH_SECRET_KEY
+  );
+};
+
+const createUser = async (req, res, next) => {
+  let user = await User.findOne({ where: { email: req.body.email } });
+
+  if (user !== null)
+    return res
+      .status(500)
+      .send({ message: "A user has already registered with this email." });
+
+  user = await User.create(req.body);
+
+  if (!(user && user.id))
+    return res.status(500).send({ message: "Unable to process the request." });
+
+  const token = signToken(user.id);
+  res.status(201).json({
+    token,
+    user,
+  });
 };
 
 module.exports = {
--- test/users.js
+++ test/users.js
@@ -2,6 +2,8 @@ const app = require("../app");
 const faker = require("faker");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const JWT = require("jsonwebtoken");
+const { User } = require("../models");
 
 const { expect } = chai;
 chai.use(chaiHttp);
@@ -20,13 +22,38 @@ describe("Users", () => {
       .post("/users/")
       .send(data)
       .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body.name).to.equals(data.name);
-        expect(res.body.email).to.equals(data.email);
-        expect(res.body.password).to.equals(data.password);
-        expect(res.body.contact).to.equals(data.contact);
-        expect(res.body.avatar).to.equals(data.avatar);
+        expect(res).to.have.status(201);
+        const decoded = JWT.verify(res.body.token, process.env.AUTH_SECRET_KEY);
+        expect(res.body.user.id).to.equals(decoded.sub);
+        expect(res.body.user.name).to.equals(data.name);
+        expect(res.body.user.email).to.equals(data.email);
+        expect(res.body.user.password).to.equals(data.password);
+        expect(res.body.user.contact).to.equals(data.contact);
+        expect(res.body.user.avatar).to.equals(data.avatar);
         done();
       });
+  });
+
+  it("User Duplication Error on create user", (done) => {
+    const data = {
+      name: faker.name.firstName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      contact: faker.phone.phoneNumber(),
+      avatar: faker.image.imageUrl(),
+    };
+    User.create(data).then(() => {
+      chai
+        .request(app)
+        .post("/users/")
+        .send(data)
+        .end((err, res) => {
+          expect(res).to.have.status(500);
+          expect(res.body.message).to.equals(
+            "A user has already registered with this email."
+          );
+          done();
+        });
+    });
   });
 });
